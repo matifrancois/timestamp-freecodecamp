83ba4c5c694c7dd0ba0b97b5b92c9439a6387e14
Matias Francois <mfrancois@itba.edu.ar>
2021-05-15 01:13:22 -0300

    now time with /api

--- server.js
+++ server.js
@@ -15,17 +15,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-// app.get("/api/:date_string", function (req, res) {
-//   let dateString = req.params.date_string;
-//   let ValueDate = new Date(dateString);
-//   console.log(ValueDate);
-//   console.log(dateString);
+app.get("/api", function (req, res) {
+  let now = new Date();
 
-//   res.json({
-//     "unix": ValueDate.getTime(),
-//     "utc": ValueDate.toUTCString()
-//   });
-// });
+  res.json({
+    "unix": now.getTime(),
+    "utc": now.toUTCString()
+  });
+});
+
 
 app.get("/api/:date_string", function (req, res) {
   let dateString = req.params.date_string;
